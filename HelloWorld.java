0c60a40391dfbb8020ada022a741f1165338811d
sriharshaklu <57980261+sriharshaklu@users.noreply.github.com>
2022-02-11 16:12:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      int num1 = 55, num2 = 15, sum,sub;
     sum = num1 + num2;
-    sub = num1-num2;
+    sub = num1 - num2;
     System.out.println("Subtraction of 2 numbers: "-sub);
     System.out.println("Sum of these numbers: "+sum);
     System.out.println("Hello, World");

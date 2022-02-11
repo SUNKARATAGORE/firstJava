c6980d252483a9eef6f676841c133c0247adcd26
sriharshaklu <57980261+sriharshaklu@users.noreply.github.com>
2022-02-11 16:09:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
+     int num1 = 5, num2 = 15, sum;
+    sum = num1 + num2;
+    System.out.println("Sum of these numbers: "+sum);
     System.out.println("Hello, World");
     System.out.println("Build, Test, Deploy");
     System.out.println("Hi Harsha");
